c5d462ce5ebd66fa76c58e10f6c9df8df6b1c467
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-03-09 19:02:03 +0300

    Update avl-tree header file

--- src/avl_tree/avl_tree.h
+++ src/avl_tree/avl_tree.h
@@ -1,35 +1,59 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdlib.h>
 
 typedef struct AVLTree AVLTree;
 
-// Creates AVL-tree with 3 symbol string keys and string values and returns pointer to it.
-AVLTree* avlTreeCreate();
-
-// Deletes AVL-tree and frees memory used by it.
-void avlTreeFree(AVLTree* tree);
+/*
+ * Creates AVL-tree with string keys and string values and returns pointer to it.
+ */
+AVLTree* avlTreeCreate(void);
 
 /*
-Finds node with given key.
-Returns this node pointer on the string.
-If key does not exists returns NULL.
-*/
+ * Deletes AVL-tree, frees memory used by it and NULLs pointer.
+ * Returns true if all correct and false if somethong went wrong.
+ */
+bool avlTreeFree(AVLTree** tree);
+
+/*
+ * Finds node with given key.
+ * Returns this node pointer to the value.
+ * If something went wrong returns NULL.
+ */
 char* avlTreeFind(AVLTree* tree, char* key);
 
 /*
-If given key does not exists, adds node with given key and value in tree and returns true.
-If key is already exists, returns false.
-*/
+ * Finds height of tree and writes it in 'height' variable.
+ * Height - count of not empty layers of nodes.
+ * If something went wrong returns false, else returns true.
+ */
+bool avlTreeHeight(AVLTree* tree, size_t* height);
+
+/*
+ * Finds size of tree and writes it in 'size' variable.
+ * Size - count of nodes in tree.
+ * If something went wrong returns false, else returns true.
+ */
+bool avlTreeSize(AVLTree* tree, size_t* size);
+
+/*
+ * If given key does not exists, adds node with given key and value in tree and returns true.
+ * If key is already exists or something went wrong returns false.
+ */
 bool avlTreeAdd(AVLTree* tree, char* key, char* value);
 
 /*
-Deletes node with given key and returns true, if node is exists.
-If node with given key does not exists, returns false.
-*/
-bool avlTreeDelete(AVLTree* tree, char* key);
+ * Adds in tree nodes with keys and values from file <filename>.
+ * Keys and values in file must be in format '<key>:<value>\n'.
+ * <key> and <value> must not include ':' and '\n'.
+ * If node with the <key> already exists in tree, skips it.
+ * If something went wrong returns false, else returns true.
+ */
+bool avlTreeAddFromFile(AVLTree* tree, char* filename);
 
 /*
-Prints tree.
-*/
-void avlTreePrint(AVLTree* tree);
+ * Deletes node with given key and returns true, if node is exists.
+ * If node with given key does not exists or something went wrong returns false.
+ */
+bool avlTreeDelete(AVLTree* tree, char* key);
